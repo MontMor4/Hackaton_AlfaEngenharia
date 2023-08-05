32002f29d75ffbb25db2aa9f776e9d4b914629d8
Luiz Eduardo Leroy <leroy291001@gmail.com>
2023-08-05 12:52:30 -0300

    backend atualizado

--- alfa/server/index.js
+++ alfa/server/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mysql = require("mysql");
+const mysql = require("mysql2");
+const bodyParser = require('body-parser')
+
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "password",
+  password: "luiz2001",
   database: "portal_externo",
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(express.json());
 app.use(cors());
@@ -30,9 +37,9 @@ app.get("/", (req, res) => {
 });
 
 app.post("/cadastrar-candidato", (req, res) => {
-  //res.send({'corpo':req.body})
   try {
     const params = req.body;
+    /*
     properties = [
       "nome",
       "nomepai",
@@ -84,33 +91,62 @@ app.post("/cadastrar-candidato", (req, res) => {
     if (!check) {
       console.log("Alguns parametros não foram enviados");
     }
-  } catch (error) {
-    res.send(error);
-  }
-  if (!req.body) {
-    console.log(req.body);
-    res.send({ resposta: "a requisição está vazia" });
-  }
-  const newUser = {
-    id: req.body.id,
-    nome: req.body.nome,
-    email: req.body.email,
-    senha: req.body.senha,
-    nickname: req.body.nickname,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-
-  pool.query(
-    "INSERT INTO usuarios SET ?",
-    newUser,
-    (error, results, fields) => {
-      if (error) {
-        console.error("Error inserting data:", error);
-      } else {
-        console.log("Data inserted successfully:", results);
-        res.send({ resposta: results });
-      }
+    */
+    if (!req.body) {
+      console.log(req.body);
+      res.send({ resposta: "a requisição está vazia" });
     }
-  );
+    const newCandidato = {
+      nomecandidato:req.body.nomecandidato,
+      nomemae:req.body.nomemae,
+      nomepai:req.body.nomepai,
+      sexo:req.body.sexo,
+      estadocivil:req.body.estadocivil,
+      graudeinstrucao:req.body.graudeinstrucao,
+      racacor:req.body.racacor,
+      cep:req.body.cep,
+      pais:req.body.pais,
+      estado:req.body.cidade,
+      bairro:req.body.bairro,
+      tipologradouro:req.body.tipologradouro,
+      enderecoresidencial:req.body.enderecoresidencial,
+      numero:req.body.numero,
+      complementoendereco:req.body.complementoendereco,
+      rg:req.body.rg,
+      orgaoemissor:req.body.orgaoemissor,
+      cidadeemissaorg:req.body.cidadeemissaorg,
+      dataexpedicao:req.body.dataexpedicao,
+      cpf:req.body.cpf,
+      tamanhobotina:req.body.tamanhobotina,
+      tamanhocalca:req.body.tamanhocalca,
+      tamanhocamisa:req.body.tamanhocamisa,
+      datanascimento:req.body.datanascimento,
+      nacionalidade:req.body.nacionalidade,
+      paisnascimento:req.body.paisnascimento,
+      estadonascimento:req.body.estadonascimento,
+      cidadenascimento:req.body.cidadenascimento,
+      telefone1:req.body.teefone1,
+      telefone2:req.body.telefone2,
+      email:req.body.email,
+    };
+  
+    db.query(
+      "INSERT INTO candidato SET ?",
+      newCandidato,
+      (error, results, fields) => {
+        if (error) {
+          console.error("Error inserting data:", error);
+          res.send({"erro":error})
+        } else {
+          console.log("Data inserted successfully:", results);
+          res.send({ "resposta": results });
+        }
+      }
+    );
+
+  } catch (error) {
+    console.log(error);
+    //res.send({"erro":error});
+  }
+  
 });
